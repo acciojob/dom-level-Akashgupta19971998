6a4e5a2911936eeb4a23d348cb8197d986e2601b
Akashgupta19971998 <akashguptaakash2@gmail.com>
2023-04-13 13:01:53 +0530

    commit

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let ans=document.getElementById("level")
+
+function op(){
+    if(ans){
+        alert("The level of the element is: "+ans.innerHTML)
+    }
+}
+op()
